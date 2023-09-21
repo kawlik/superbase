704e35a999492b812e4779a7d3e523945df1952b
kpawlik <kamil.pawlik@capgemini.com>
2023-09-21 10:07:56 +0200

    Sign In works! But is not impressive...

--- src/app/auth/sign-in/page.login.tsx
+++ src/app/auth/sign-in/page.login.tsx
@@ -1,29 +1,30 @@
 // @node
-import { useState } from "react";
 import { useForm } from "react-hook-form";
 
-export function Login(props: { signInWithEmail(email: string): void }) {
+export function Login(props: { signInWithEmail(email: string): Promise<void> }) {
 	// component hooks
 	const form = useForm<{ email: string }>();
 
 	// component logic
-	const [isLoading, setIsLoading] = useState(false);
+	const isInvalid = !form.formState.isValid;
+	const isLoading = form.formState.isSubmitting;
+	const isDisabled = isInvalid || isLoading;
+
+	const submitForm = form.handleSubmit(async ({ email }) => {
+		await props.signInWithEmail(email);
+	});
 
 	// component layout
 	return (
-		<form className="flex w-full flex-col gap-2">
+		<form className="flex w-full flex-col gap-2" onSubmit={submitForm}>
 			<input
 				{...form.register("email", { required: true })}
 				className="input input-bordered w-full"
 				placeholder="Enter your e-mail address"
 				type="email"
 			/>
-			<button
-				className="btn btn-primary normal-case"
-				disabled={!form.formState.isValid}
-				type="submit"
-			>
-				Sign In
+			<button className="btn btn-primary normal-case" disabled={isDisabled} type="submit">
+				{isLoading ? <span className="loading loading-dots loading-lg" /> : "Sign In"}
 			</button>
 		</form>
 	);
--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,11 +1,22 @@
 "use client";
 
+// @root
+import { useAuthContext } from "@/contexts/auth";
+
 // @view
 import { Banner } from "./page.banner";
 import { Login } from "./page.login";
 
 export default function () {
+	// component hooks
+	const authCtx = useAuthContext();
+
 	// component logic
+	const signInWithEmail = async (email: string) => {
+		await authCtx.auth.signInWithOtp({ email }).then((res) => {
+			console.log(res);
+		});
+	};
 
 	// component layout
 	return (
@@ -16,7 +27,7 @@ export default function () {
 					<h1 className="text-4xl font-bold">Hello, Stranger!</h1>
 					<p className="text-lg font-thin">Maybe you want to log in?</p>
 				</div>
-				<Login signInWithEmail={() => {}} />
+				<Login signInWithEmail={signInWithEmail} />
 			</div>
 		</div>
 	);
